0947d423efd75e184391eac4f4fa77e8d17101e6
churchesyt <elcracknelson@gmail.com>
2026-05-03 20:38:01 -0500

    fix

--- src/es/tmohentai/src/eu/kanade/tachiyomi/extension/es/tmohentai/TMOHentaiUrlActivity.kt
+++ src/es/tmohentai/src/eu/kanade/tachiyomi/extension/es/tmohentai/TMOHentaiUrlActivity.kt
@@ -8,7 +8,7 @@ import android.util.Log
 import kotlin.system.exitProcess
 
 /**
- * Springboard that accepts https://tmohentai.com/contents/:id intents and redirects them to
+ * Springboard that accepts https://tmohentai.app/contents/:id intents and redirects them to
  * the main Tachiyomi process.
  */
 class TMOHentaiUrlActivity : Activity() {
